7272b5bfbbbdb8bb178163e3416600134421d3c9
Ricardo Antonio Gomez Villalobos <bardack134@gmail.com>
2024-02-22 18:26:16 +0900

    ciclo for que recorre la lista user_data_list

--- main.py
+++ main.py
@@ -12,14 +12,36 @@ window.title("To-Do App")
 EnterYourTask_label=Label(window, text='Enter your task', bg="black", fg="white", font=('calibre', 14, 'bold'))
 EnterYourTask_label.pack(side=TOP)
 
+#lista donde se guardaran las tareas ingresadas por el usuario
+user_data_list=[]
+
+
+enter_data=StringVar()
+
+#funcion que se ejecutara cuando el usario oprima el boton submit
+def submit():
+    #usando el metodo get obtenemos la informacion ingresada por el user
+    enter_data_get=enter_data.get()
+    
+    #agregamos la tarea a la lista
+    user_data_list.append(enter_data_get)
+    
+    #imprimo en consola para saber si estamos　leyendo la informacion ingresada por el user correctamente
+    print(user_data_list)
+    
+    #reccoro con un ciclo for la lista de tareas del user 'user_data_list'
+    for i in user_data_list:
+        
+        #con el metodo insert, agrego la informacion escrita por el usuario al text_widget
+        text_widget.insert(INSERT, f"{i}\n")
 
 #Entry widget que el usuario usara para ingresar la tarea
-data_entry_widget=Entry(window, width=30, font=("Helvetica", "14") )
+data_entry_widget=Entry(window, textvariable=enter_data, width=30, font=("Helvetica", "14") )
 data_entry_widget.pack( pady=7, padx=7)
 
 
 #Boton de submit
-submit_button=Button(window, text='Submit', )
+submit_button=Button(window, text='Submit', command=submit )
 submit_button.pack(pady=7)
 
 
@@ -27,6 +49,7 @@ submit_button.pack(pady=7)
 text_widget=Text(window, height=10, width=30, font=("Helvetica", "17"))
 text_widget.pack(pady=7, padx=7)
 
+# text_widget.insert(INSERT, "esto es una prueba")
 
 #label dindicando al usuario que ingrese el numero de la tarea a eliminar
 elimanate_task_label=Label(window, text='Enter the task number to delete', bg="black", fg="white", font=('calibre', 14, 'bold'))
