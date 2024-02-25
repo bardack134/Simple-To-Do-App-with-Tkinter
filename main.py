ae2aef9674d7cbdcab9e1f5f3a78e6fa6439327a
Ricardo Antonio Gomez Villalobos <bardack134@gmail.com>
2024-02-26 07:06:12 +0900

    completar tarea boton, funcionando

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 
 #creamos ventana
 window=Tk()
@@ -34,7 +35,7 @@ def submit():
     print(user_data_list)
     
     #con el metodo insert, agrego la informacion escrita por el usuario al text_widget
-    text_widget.insert(INSERT, f"[{position}]{enter_data_get}\n")
+    list_widget.insert(position, f"[{position}] {enter_data_get}\n")
     
     #aumento la posicion en 1
     position +=1
@@ -53,10 +54,15 @@ submit_button.pack(pady=7)
 
 
 #Donde se mostraran las tareas que el usuario vaya agregando
-text_widget=Text(window, height=10, width=30, font=("Helvetica", "17"))
-text_widget.pack(pady=7, padx=7)
-
-
+# text_widget=Text(window, height=10, width=30, font=("Helvetica", "17"))
+# text_widget.pack(pady=7, padx=7)
+list_widget=Listbox(
+                  bg = "black",
+                  activestyle = 'dotbox', 
+                  font = "Helvetica",
+                  fg = "yellow",
+                  selectmode=MULTIPLE)
+list_widget.pack(pady=7, padx=7)
 
 #label dindicando al usuario que ingrese el numero de la tarea a eliminar
 elimanate_task_label=Label(window, text='Enter the task number to delete', bg="black", fg="white", font=('calibre', 14, 'bold'))
@@ -66,12 +72,21 @@ elimanate_task_label.pack(side=TOP)
 delete_data=StringVar()
 
 def deleteTask():
-    global user_data_list
-    delete_data_get=delete_data.get()
+    if list_widget.size()==0:
+        print("No task")
+        messagebox.showerror("No task")
+    else:
+        delete_data_get=delete_data.get()
+        
+        list_widget.delete(delete_data_get)
+
+def endTask():
+
+    for i in list_widget.curselection():
+        #  la sgt linea  da el texto asociado a ese índice i
+        item = list_widget.get(i)
+        completed_task_list.insert(position, f"{item}  OK")
     
-    text_widget.delete(delete_data_get)
-
-
 #Entry widget que el usuario usara para eliminar una tarea
 eliminate=Entry(window, textvariable=delete_data, width=5, font=("Helvetica", "15") )
 eliminate.pack( pady=7,)
@@ -81,8 +96,23 @@ eliminate.pack( pady=7,)
 delete_button=Button(window, command=deleteTask, text='Delete', )
 delete_button.pack(pady=7)
 
+#label dindicando al usuario que seleccione las tareas que desea completar
+complete_task_label=Label(window, text="Please select the tasks you have completed and click the 'Complete Task' button.",
+                        bg="black", fg="white", font=('calibre', 14, 'bold'))
+complete_task_label.pack()
 
 
+#Completar tatera Botton
+task_completed_button=Button(window, command=endTask, text='complete task', )
+task_completed_button.pack(pady=7)
+
+#Donde se mostraran las tareas que el usuario ha completado
+completed_task_list=Listbox(
+                  bg = "black",
+                  activestyle = 'dotbox', 
+                  font = "Helvetica",
+                  fg = "yellow")
+completed_task_list.pack(pady=7, padx=7)
 
 
 #bucle para que la ventana permaniezca abierta
